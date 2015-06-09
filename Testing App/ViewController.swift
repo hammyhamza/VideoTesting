117b8ede49fcce5c07c38c2894d3bfc91af261ab
Hamza Ansari <hamzaansari@Thoughts-Mac-mini.local>
2015-06-09 17:38:54 +0530

    Initial Commit

---
+++ Testing App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing App
+//
+//  Created by Hamza Ansari on 09/06/15.
+//  Copyright (c) 2015 Thought Chimp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
